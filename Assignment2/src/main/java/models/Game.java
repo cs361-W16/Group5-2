b66e5c4b976f9201e711590bdd9a2ae2ec85ef78
burnettn@onid.oregonstate.edu <burnettn@onid.oregonstate.edu>
2016-02-03 19:47:21 -0800

    Removed Spaces

--- Assignment2/src/main/java/models/Game.java
+++ Assignment2/src/main/java/models/Game.java
@@ -22,40 +22,6 @@ public class Game {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    //I like space between my methods
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public void buildDeck() {
         for(int i = 2; i < 15; i++){
             deck.add(new Card(i,Suit.Clubs));
@@ -64,31 +30,6 @@ public class Game {
             deck.add(new Card(i,Suit.Spades));
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-    //I like space between my methods
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     public void shuffle() {
